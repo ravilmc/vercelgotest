60778d4eb9c90e8720dd2c1304a95f667d7f5411
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-02-10 11:24:16 +0545

    test rewrite v2

--- api/index.go
+++ api/index.go
@@ -3,8 +3,21 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World</h1>")
+func NewRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	}).Methods("GET")
+	return r
+}
+
+
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router := NewRouter()
+	router.ServeHTTP(w, r)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ravilmc/vercelgotest
 
 go 1.21.5
+
+require github.com/gorilla/mux v1.8.1
