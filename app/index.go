16fd063db8652b94b8cc8ef1621c723ec37201b9
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-02-10 11:06:20 +0545

    app test

--- app/index.go
+++ app/index.go
@@ -5,6 +5,6 @@ import (
 	"net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
