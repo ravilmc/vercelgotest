8d88f95051c52873bb05fd662446372429ddd592
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-03-25 16:02:49 +0545

    test 2

--- api/index.go
+++ api/index.go
@@ -9,13 +9,22 @@ import (
 
 func NewRouter() *mux.Router {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "You have found light at the end of the tunnel!")
+	})
+
+	r.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "Hello Your ID is %s", id)
+	})
+
+	r.HandleFunc("GET /api/v1/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
-	}).Methods("GET")
+	})
+
 	return r
 }
-
-
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	router := NewRouter()
