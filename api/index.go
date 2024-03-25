2fba1c745e5e4ab3c379b85e453044cefdf218e4
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-03-25 16:15:46 +0545

    region test

--- api/index.go
+++ api/index.go
@@ -3,27 +3,20 @@ package handler
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-func NewRouter() *mux.Router {
-	r := mux.NewRouter()
+func NewRouter() *http.ServeMux {
+	router := http.NewServeMux()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "You have found light at the end of the tunnel!")
-	})
-
-	r.HandleFunc("GET /{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		fmt.Fprintf(w, "Hello Your ID is %s", id)
-	})
-
-	r.HandleFunc("GET /api/v1/health", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
 
-	return r
+	router.HandleFunc("/api/{version}/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "API Version: %s", r.PathValue("version"))
+	})
+
+	return router
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,14 +1,3 @@
 module github.com/ravilmc/vercelgotest
 
 go 1.22.1
-
-require github.com/gorilla/mux v1.8.1
-
-require (
-	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
-	github.com/klauspost/compress v1.15.15 // indirect
-	github.com/libsql/sqlite-antlr4-parser v0.0.0-20230802215326-5cb5bb604475 // indirect
-	github.com/tursodatabase/libsql-client-go v0.0.0-20240208053015-5d6aa1e2196d // indirect
-	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
-	nhooyr.io/websocket v1.8.7 // indirect
-)
