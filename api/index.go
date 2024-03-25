45384ada2681e9430df97d165649511a9d0d199c
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-03-25 17:26:20 +0545

    separate file test

--- api/index.go
+++ api/index.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"net/http"
+
+	"github.com/ravilmc/vercelgotest/internal/handler"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	handler := NewHandler()
+	handler := handler.NewHandler()
 	handler.ServeHTTP(w, r)
 }
