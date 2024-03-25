c51926613aa8476acd22158c01d2334e606dc598
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-03-25 17:21:49 +0545

    separate file test

---
+++ api/handler.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type ApiHandler struct {
+}
+
+func (h *ApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	api := http.NewServeMux()
+	api.Handle("/api/", http.StripPrefix("/api", NewRouter()))
+	api.ServeHTTP(w, r)
+}
+
+func NewHandler() *ApiHandler {
+	return &ApiHandler{}
+}
+
+func NewRouter() *http.ServeMux {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+	router.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World post")
+	})
+
+	router.HandleFunc("GET /{version}/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "API Version: %s", r.PathValue("version"))
+	})
+
+	return router
+}
--- api/index.go
+++ api/index.go
@@ -1,31 +1,10 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func NewRouter() *http.ServeMux {
-	router := http.NewServeMux()
-
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	})
-	router.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World post")
-	})
-
-	router.HandleFunc("GET /{version}/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "API Version: %s", r.PathValue("version"))
-	})
-
-	return router
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
-	api := http.NewServeMux()
-
-	api.Handle("/api/", http.StripPrefix("/api", NewRouter()))
-
-	api.ServeHTTP(w, r)
+	handler := NewHandler()
+	handler.ServeHTTP(w, r)
 }
